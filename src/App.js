731d2f0ea793fae6e6f6b89876358de0b4342a74
Viraj Prajapati <prajapativiraj234@gmail.com>
2022-03-12 12:56:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
-import "./App.css";
-import { List } from "./Components/Lists/List";
-import Navbar from "./Components/Navbar/Navbar";
 import React, { useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import TodoList from "./Components/TodoList";
 
 const App = () => {
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState("");
+  const [mytask, setMyTask] = useState([]);
 
-  const onChangeInput = (e) => {
-    const newTask = e.target.value;
-    setTask(newTask);
+  const handleInput = (e) => {
+    setTask(e.target.value);
   };
 
-  const [myTask, setMyTask] = useState([]);
-
-  const onSubmitHandler = () => {
-    const temp = task;
-    setMyTask([...myTask, temp]);
-    setTask([]);
+  const handleOnClick = () => {
+    task.length === 0
+      ? alert("Enter Your Task!")
+      : setMyTask((oldTasks) => {
+          return [...oldTasks, task];
+        });
+    setTask("");
   };
 
-  const onDeleteHandler = (index) => {
-      console.log(index);
+  const handleOnDelete = (id) => {
+    setMyTask(
+      mytask.filter((items, index) => {
+        return id !== index;
+      })
+    );
   };
 
   return (
     <>
-      <div>
-        <Navbar size={myTask.length} />
-      </div>
-      <div>
-        <List
-          Task={task}
-          myTask={myTask}
-          onSubmit={onSubmitHandler}
-          onChangeInput={onChangeInput}
-          onDelete={(index) => onDeleteHandler.bind(this, index)}
-        />
+      <div className="outer_container">
+        <div className="todo_container">
+          <h1>Todo-List</h1>
+          <br />
+          <div className="todo_main">
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Enter Your Task"
+                value={task}
+                onChange={handleInput}
+              />
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+                onClick={handleOnClick}
+              >
+                Button
+              </button>
+            </div>
+            <br />
+            <ul>
+              {mytask.map((currItem, index) => {
+                return (
+                  <TodoList
+                    item={currItem}
+                    key={index}
+                    id={index}
+                    delete={handleOnDelete}
+                  />
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
